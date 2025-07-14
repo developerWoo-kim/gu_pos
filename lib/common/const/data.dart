a81655697b176f7973d9cfbdba74ba1d54f90e56
gunwookim <gunwo151@gmail.com>
2025-07-14 23:37:34 +0900

    data 컴포넌트

---
+++ lib/common/const/data.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN';
+const REFRESH_TOKEN_KEY = 'REFRESH_TOKEN';
+
+// localhost 에뮬레이터 기준
+final emulatorIp = 'https://cdac3cf5723a.ngrok-free.app';
+// final emulatorIp = 'https://41a0-116-36-82-239.ngrok-free.app';
+// localhost 시뮬레이터 기준
+final simulatorIp = 'https://4532-116-36-82-239.ngrok-free.app';
+// final simulatorIp = 'https://f400-116-36-82-239.ngrok-free.app';
+
+final ip = Platform.isIOS ? simulatorIp : emulatorIp;
